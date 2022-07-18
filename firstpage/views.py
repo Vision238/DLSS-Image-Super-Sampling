27bd8e21451ef56910786cd7c15cdd18b16ce635
Vision <123aggarwalvision@gmail.com>
2022-07-18 16:25:09 +0530

    Added Dataset

--- firstpage/views.py
+++ firstpage/views.py
@@ -21,12 +21,12 @@ def predict(request):
     #  if(png):
     #             x = x[...,:3]
     img=img[...,:3]
-    img = cv2.resize(img,(360,640))
+    img = cv2.resize(img,(128,128))
     x = np.array(img)
     generator=load_model('models\generator.h5')
-    x = np.reshape(x,(1,360,640,3))
+    x = np.reshape(x,(1,128,128,3))
     gen_imgs = generator.predict(x)
-    a = np.reshape(gen_imgs,(720,1280,3))
+    a = np.reshape(gen_imgs,(256,256,3))
     a = image.array_to_img(a)
     no = np.random.normal(0,1, (1, 1))
     no = no[0][0]
